570b62c2f6fc24649cc5cf475aaa09c740aaecc2
kimdolion <kimowilkes@gmail.com>
2023-01-19 15:42:49 -0500

    Add react-stripe-js

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,12 +2,13 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
+  <Layout title="Pokemon Marketplace">
+    <h1>About Pokemon Marketplace</h1>
     <p>This is the about page</p>
-    <p>
+    <div>
+      <Link href="https://pokeapi.co/">Go to the PokeAPI</Link>
       <Link href="/">Go home</Link>
-    </p>
+    </div>
   </Layout>
 )
 
