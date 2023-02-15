6ffb1c51619932bc1aea3e8f19eef14df8324878
kimdolion <kimowilkes@gmail.com>
2023-02-15 01:16:21 -0500

    Add react-axe and update to include _document for html lang

---
+++ pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <link rel="shortcut icon" type="image/jpg" href="kor_am_flag.jpg" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
